e8195d3c0d081fab0a1f1fb7b511a1451e1141b9
1306080249 <1306080249@qq.com>
2019-12-06 10:53:24 +0800

    提交home.vue文件

---
+++ home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="height: 100%">
+    just test
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      
+    },
+    mounted() {
+      
+    },
+    methods: {
+      
+    },
+    destroyed() {
+      
+    }
+  }
+</script>
+<style scoped>
+  
+</style>
